9513b58e057d95e32665063bd6a3d31aea901908
Britney <britney.rideaux@yahoo.com>
2023-04-04 22:38:25 -0500

    problems

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,10 +1,8 @@
-// Your code here
-
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,11 +1,23 @@
-// Your code here
-
+class PartyPlanner {
+  constructor() {
+    this.guestList = [];
+  }
+  addToGuestList(name) {
+    this.guestList.push(name);
+  }
+  throwParty() {
+    if (this.guestList === 0) {
+      return "Gotta add people to the guest list";
+    } else {
+    }
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = PartyPlanner;
+  module.exports = PartyPlanner;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
